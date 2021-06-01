dd00ce6398076e86037ca747a6973ff4a30b720f
pa-kuuuuk <parkjungyong0522@gmail.com>
2021-06-02 00:25:48 +0900

    정말 하면 할수록 어렵다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component{
   
@@ -24,11 +25,39 @@ class App extends Component{
     })
   }
 
+  handleRemove = (id) => {
+    const {information} = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+
+  handleUpdate = (id,data) => {
+    const {information} = this.state;
+    this.setState({
+      information: information.map(
+        info => {
+          if(info.id === id){
+            return{
+              id,
+              ...data,
+            };
+          }
+          return info;
+        }
+      )
+    });
+  }
+
   render(){
     return(
       <div>
         <PhoneForm onCreate={this.handleCreate}/>
-        {JSON.stringify(this.state.information)}
+        <PhoneInfoList 
+        data={this.state.information}
+        onRemove ={this.handleRemove}
+        onUpdate={this.handleUpdate}
+        />  
       </div>
     )
   }
--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -1,10 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class PhoneInfo extends Component {
+    state ={
+        editing: false,
+        name:'',
+        phone:'',
+    }
+
+    handleRemove = () => {
+        const {info, onRemove} = this.props;
+        onRemove(info.id);
+    }
+
+    handleToggleEdit = () => {
+        const {info, onUpdate} = this.props;
+        if(this.state.editing){
+            onUpdate(info.id,{
+                name: this.state.name,
+                phone: this.state.phone
+            });
+        }else{
+            this.setState({
+                name: info.name,
+                phone: info.phone,
+            });
+        }
+
+        this.setState({
+            editing: !this.state.editing,
+        })
+    }
+
+    handleCharnge = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
     render() {
+        const {name, phone, id} = this.props.info;
+        const {editing} = this.state;
+
+        const style ={
+            border: '1px solid black',
+            padding: '8px',
+            margin: '8px',
+
+        };
+
         return (
-            <div>
-                
+            <div style={style}> 
+                {
+                    editing ? (
+                            <Fragment>
+                                <div><input name='name' onChange={this.handleCharnge} value={this.state.name}/></div>
+                                <div><input name='phone' onChange={this.handleCharnge} value={this.state.phone}/></div>
+                            </Fragment>
+                        ):(
+                        <Fragment>
+                        <div><b>{name}</b></div>
+                        <div>{phone}</div>
+                        <div>{id}</div>
+                        </Fragment>
+                    )
+                }
+                <button onClick={this.handleRemove}>삭제</button>
+                <button onClick={this.handleToggleEdit}>
+                    {editing ? '적용' : '수정'}
+                </button>
             </div>
         );
     }
